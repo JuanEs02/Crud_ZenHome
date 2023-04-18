5b3f509e12bb8e864f88a0ddca6add0a00c45845
JuanEs02 <114095229+JuanEs02@users.noreply.github.com>
2023-04-17 20:46:06 -0500

    Listado y Eliminar terminados

--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { db } from '../firebase'
-import { collection, doc, addDoc, onSnapshot, deleteDoc, updateDoc } from 'firebase/firestore'
+import { collection, doc, addDoc, onSnapshot, deleteDoc, updateDoc, query } from 'firebase/firestore'
 
 const Formulario = () => {
 
@@ -16,6 +16,19 @@ const Formulario = () => {
     const [nacimiento, setNacimiento] = useState('')
     const [imagenaleatoria, setImagenAleatoria] = useState('https://picsum.photos/200/299')
 
+    useEffect(() => {
+        const obtenerDatos = async () => {
+            try {
+                await onSnapshot(collection(db, 'usuarioszenhome'), (query) => {
+                    setListaUsuarios(query.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+                })
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        obtenerDatos();
+    }, [])
+
     const guardarUsuario = async (e) => {
         e.preventDefault()
         try{
@@ -28,10 +41,39 @@ const Formulario = () => {
                 telefono: telefono,
                 nacimiento: nacimiento
             })
+        setListaUsuarios(
+            [...listaUsuarios,{
+                id: data.id,
+                usuario: usuario,
+                nombre: nombre,
+                NID: NID,
+                correo: correo,
+                direccion: direccion,   
+                telefono: telefono,
+                nacimiento: nacimiento
+            }]
+        )
+            setUsuario('')
+            setNombre('')
+            setNID('')
+            setCorreo('')
+            setDireccion('')
+            setTelefono('')
+            setNacimiento('')
+
         }catch(error){
             console.log(error)
         }
     }
+
+    const eliminar = async id => {
+        try {
+            await deleteDoc(doc(db, 'usuarioszenhome', id))
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <div className='container mt-5'>
             <h1 className="text-center">Zen Home</h1>
@@ -40,6 +82,14 @@ const Formulario = () => {
                 <div className="col-8">
                     <h4 className="text-center">Lista de Usuarios</h4>
                     <ul className="list-group">
+                        {
+                            listaUsuarios.map(item =>(
+                                <li className="list-group-item" key={item.id}>
+                                    <span className="lead">{item.usuario}-{item.nombre}-{item.NID}-{item.correo}-{item.direccion}-{item.telefono}-{item.nacimiento}</span>
+                                    <button className='btn btn-danger btn-sn fload-end mx-2' onClick={() => eliminar(item.id)}>Eliminar</button>
+                                </li>
+                            ))
+                        }
                         <li className="list-group-item">
                             Item temporal
                         </li>
